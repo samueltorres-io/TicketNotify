9314769e2ef51e4ee97825cd1e298f9d008643e4
samueltorres-io <torresfernandessamuel@gmail.com>
2025-11-24 14:37:00 -0300

    Adding schema of database

---
+++ ticketnotify/src/main/resources/database/schema.sql
@@ -0,0 +1,42 @@
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL, -- BCrypt
+    phone VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE roles (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(50) UNIQUE NOT NULL,
+    description VARCHAR(255)
+);
+
+CREATE TABLE user_roles (
+    user_id UUID REFERENCES users(id),
+    role_id UUID REFERENCES roles(id),
+    PRIMARY KEY (user_id, role_id)
+);
+
+CREATE TABLE events (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    date_event TIMESTAMP NOT NULL,
+    banner_url VARCHAR(255),
+    price DECIMAL(10, 2) NOT NULL,
+    owner_id UUID REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE tickets (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users(id),
+    event_id UUID REFERENCES events(id),
+    status VARCHAR(20) NOT NULL DEFAULT 'PENDING', -- Enum: PENDING, PAID, CANCELED
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
