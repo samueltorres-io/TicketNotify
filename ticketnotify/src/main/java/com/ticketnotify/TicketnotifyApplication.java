835a0c72caa0a00f55cb2d70a627af42a34749f0
samueltorres-io <torresfernandessamuel@gmail.com>
2025-11-24 10:25:05 -0300

    Creating Spring project

---
+++ ticketnotify/src/main/java/com/ticketnotify/TicketnotifyApplication.java
@@ -0,0 +1,13 @@
+package com.ticketnotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketnotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketnotifyApplication.class, args);
+	}
+
+}
---
+++ ticketnotify/src/test/java/com/ticketnotify/TicketnotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketnotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketnotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
