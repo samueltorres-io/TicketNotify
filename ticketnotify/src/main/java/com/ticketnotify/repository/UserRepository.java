f9c7636d4d7d10652e1973fbefee916fc7bdd756
samueltorres-io <torresfernandessamuel@gmail.com>
2025-11-25 17:49:55 -0300

    feat: Adding optional method to try get a user by email

--- ticketnotify/src/main/java/com/ticketnotify/repository/UserRepository.java
+++ ticketnotify/src/main/java/com/ticketnotify/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.ticketnotify.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,7 @@ import com.ticketnotify.entity.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 
 }
